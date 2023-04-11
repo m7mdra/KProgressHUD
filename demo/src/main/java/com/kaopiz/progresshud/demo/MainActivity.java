9e56ccd5ae552b213b693b774ed7497dde1f7957
m7mdra <xm7mdrax@gmail.com>
2023-04-11 21:24:29 +0300

    migrate to AndroidX and latest versions of gradle

--- demo/src/main/java/com/kaopiz/progresshud/demo/MainActivity.java
+++ demo/src/main/java/com/kaopiz/progresshud/demo/MainActivity.java
@@ -26,11 +26,12 @@ import android.content.DialogInterface;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.kaopiz.kprogresshud.KProgressHUD;
 import com.kaopiz.progresshud.R;
